17fba6334efcf999562aa60ad6b04dd580f161fa
ClaudioFritzen <sdfritzen96@gmail.com>
2023-01-30 23:55:48 +0000

    criamos a conexão com a API do Ninjas

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const btnEl = document.getElementById('btn');
 
-function getJoke(){
-    console.log('clicked!')
+const apiKey = 'OaTjyypjJ3zi7Y0Jw/+zBA==mdcF25oLJ9AryLOk'
+
+const options = {
+    method: 'GET',
+    headers: {
+        'X-Api-Key': apiKey,
+    },
+};
+
+const apiURL = 'https://api.api-ninjas.com/v1/dadjokes?limit=1'
+
+
+async function getJoke(){
+    const response = await fetch(apiURL, options)
+    const data = response.json()
+
+
+    console.log(data)
 }
 btnEl.addEventListener('click', getJoke)
