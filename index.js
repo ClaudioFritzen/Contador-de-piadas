cc4477c2b42cad9fa5beab2dcc9eb17e612d407e
ClaudioFritzen <sdfritzen96@gmail.com>
2023-01-31 15:30:44 +0000

    ajustamos a sincronia da requisição na API

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 const btnEl = document.getElementById('btn');
 
-const apiKey = 'OaTjyypjJ3zi7Y0Jw/+zBA==mdcF25oLJ9AryLOk'
+const apiKey ="OaTjyypjJ3zi7Y0Jw/+zBA==mdcF25oLJ9AryLOk";
 
 const options = {
-    method: 'GET',
-    headers: {
+    method: "GET",
+    headers: { 
         'X-Api-Key': apiKey,
     },
 };
 
-const apiURL = 'https://api.api-ninjas.com/v1/dadjokes?limit=1'
-
+const apiURL = "https://api.api-ninjas.com/v1/dadjokes?limit=1"
 
 async function getJoke(){
-    const response = await fetch(apiURL, options)
-    const data = response.json()
+    const response = await fetch(apiURL, options);
+    const data = await response.json();
 
 
     console.log(data)
