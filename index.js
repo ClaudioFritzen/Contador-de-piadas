60e94b7c8855be7d6e623f17aab3ce10f7114cd7
ClaudioFritzen <sdfritzen96@gmail.com>
2023-01-31 21:38:53 +0000

    fizemos a validação e tratamento de erros

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const btnEl = document.getElementById('btn');
+const jokeEl = document.getElementById('joke');
+
 
 const apiKey ="OaTjyypjJ3zi7Y0Jw/+zBA==mdcF25oLJ9AryLOk";
 
@@ -12,10 +14,25 @@ const options = {
 const apiURL = "https://api.api-ninjas.com/v1/dadjokes?limit=1"
 
 async function getJoke(){
-    const response = await fetch(apiURL, options);
-    const data = await response.json();
 
-    /* recebemos da API um objeto, dentro do pararenteses é o elemento e .joke a outra coisa dentro do elemento */
-    console.log(data[0].joke);
+    try {
+        jokeEl.innerText = "Updating...";
+        btnEl.disabled = true;
+        btnEl.innerText = "Loading...";
+
+        const response = await fetch(apiURL, options);
+        const data = await response.json();
+
+        btnEl.disabled = false;
+        btnEl.innerText = "Gerar uma piada...";
+
+        /* recebemos da API um objeto, dentro do pararenteses é o elemento e .joke a outra coisa dentro do elemento */
+        //console.log(data[0].joke);
+        jokeEl.innerText = data[0].joke;
+    } catch (error) {
+        jokeEl.innerText = 'Ocorreu um erro! tente mais tarde!'
+        console.log(error);
+        
+    }  
 }
 btnEl.addEventListener('click', getJoke)
